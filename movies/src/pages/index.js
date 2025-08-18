054de039857f539ebb6d4f6ca57ab4512380f961
nonamehj <wpgus77@naver.com>
2025-08-18 23:55:18 +0900

    Updates

--- movies/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import Home from "./Home";
-import Kids from "./Kids";
-import Popular from "./Popular";
-import Comedy from "./Comedy";
-import Theatre from "./Theatre";
-import Search from "./Search";
-import Error from "./Error";
-
-export { Home, Kids, Popular, Comedy, Theatre, Search, Error };
--- movies/src/sharedLayout/index.js
+++
@@ -1,4 +0,0 @@
-import SharedLayoutSearch from "./SharedLayoutSearch";
-import SharedLayoutPage from "./SharedLayoutPage";
-import SharedLayout from "./SharedLayout";
-export { SharedLayoutSearch, SharedLayoutPage, SharedLayout };
